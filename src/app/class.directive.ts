b4d1e9b9af166b5636a53460fbdffb8551988bc3
nsohailahmed <nsohailahmed@gmail.com>
2023-04-30 10:04:36 +0530

    setting custom directive

--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -1,13 +1,19 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appClass]'
 })
 export class ClassDirective {
 
-  constructor() { 
+  
+
+  constructor(private element:ElementRef) { 
     console.log("Class directive used");
-    
+    // this.element.nativeElement.style.backgroundColor = "Red"; // to  access html element and change something in it
+  }
+
+  @Input() set backgroundColor(color:string){
+    this.element.nativeElement.style.backgroundColor = color;
   }
 
 }
