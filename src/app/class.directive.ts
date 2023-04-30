f9f777cb2410f39ecf224b1c16810eb834cf92cc
nsohailahmed <nsohailahmed@gmail.com>
2023-04-30 10:16:04 +0530

    custom directive final

--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -5,15 +5,21 @@ import { Directive, ElementRef, Input } from '@angular/core';
 })
 export class ClassDirective {
 
-  
-
   constructor(private element:ElementRef) { 
     console.log("Class directive used");
     // this.element.nativeElement.style.backgroundColor = "Red"; // to  access html element and change something in it
   }
 
-  @Input() set backgroundColor(color:string){
-    this.element.nativeElement.style.backgroundColor = color;
+  @Input("appClass") set classNames(classObj:any){
+    // for in loop to iterate keys of an object
+    for(let key in classObj){
+      if(classObj[key]){
+        this.element.nativeElement.classList.add(key);
+      }else{
+        this.element.nativeElement.classList.remove(key);
+      }
+    }
+    
   }
 
 }
